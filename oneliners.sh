820f08770d2f97afc64a016e140fdacdb3390777
Jacob Salmela <jacobsalmela@users.noreply.github.com>
2014-05-19 10:33:37 -0500

    cmds to speed up shutdown

--- oneliners.sh
+++ oneliners.sh
@@ -162,6 +162,13 @@ launchctl bslist
 # Heirarchical list of launchd processes
 launchctl bstree
 
+# Speed up shutdown time by reducing timeout values
+sudo defaults write /System/Library/LaunchDaemons/com.apple.coreservices.appleevents ExitTimeOut -int 5
+sudo defaults write /System/Library/LaunchDaemons/com.apple.securityd ExitTimeOut -int 5
+sudo defaults write /System/Library/LaunchDaemons/com.apple.mDNSResponder ExitTimeOut -int 5
+sudo defaults write /System/Library/LaunchDaemons/com.apple.diskarbitrationd ExitTimeOut -int 5
+sudo defaults write /System/Library/LaunchAgents/com.apple.coreservices.appleid.authentication ExitTimeOut -int 5
+
 ###############################
 ###### NETWORK/INTERNET #######
 
